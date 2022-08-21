6ec1913da929ef65dad7125ba054ae28a74b9283
Zofia <z.przybyl11@gmail.com>
2022-08-21 11:29:22 +0000

    Create arrow functions with map method

--- map.js
+++ map.js
@@ -5,12 +5,25 @@
  */
 
 // Using a for loop
+let nums = [1,2,3,4,5];
+let results = [];
+for (let num of nums){
+  results.push(num * 2);
+}
 
 
 // Using map()
+const multiplyByTwo = function (num){
+  return num * 2;
+}
 
+nums.map(multiplyByTwo);
 
+const simple = nums.map(multiplyByTwo);
+
+console.log(simple);
 // Simplified w/ map()
+
 
 
 // Simplfied w/ map() + arrow function
